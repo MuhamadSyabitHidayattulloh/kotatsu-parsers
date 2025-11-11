2177ba33e27e6dbfe0f5f96ba17aaccfa2d5e4b0
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2025-11-11 19:16:51 +0100

    Update MangaLivre.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MangaLivre.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MangaLivre.kt
@@ -1,13 +1,100 @@
 package org.koitharu.kotatsu.parsers.site.madara.pt
 
+import org.jsoup.nodes.Document
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
-import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.exception.ParseException
+import org.koitharu.kotatsu.parsers.model.*
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+import org.koitharu.kotatsu.parsers.util.*
 
 @MangaSourceParser("MANGALIVRE", "Manga Livre", "pt")
 internal class MangaLivre(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.MANGALIVRE, "mangalivre.tv") {
 	override val datePattern = "MMMM dd, yyyy"
 	override val withoutAjax = true
+
+	override suspend fun getListPage(page: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
+		val pages = page + 1
+
+		val url = buildString {
+			append("https://")
+			append(domain)
+
+			if (pages > 1) {
+				append("/page/")
+				append(pages.toString())
+			}
+			append("/?s=")
+
+			filter.query?.let {
+				append(filter.query.urlEncoded())
+			}
+
+			append("&post_type=wp-manga")
+
+			if (filter.tags.isNotEmpty()) {
+				filter.tags.forEach {
+					append("&genre[]=")
+					append(it.key)
+				}
+			}
+
+			filter.states.forEach {
+				append("&status[]=")
+				when (it) {
+					MangaState.ONGOING -> append("on-going")
+					MangaState.FINISHED -> append("end")
+					MangaState.ABANDONED -> append("canceled")
+					MangaState.PAUSED -> append("on-hold")
+					MangaState.UPCOMING -> append("upcoming")
+					else -> throw IllegalArgumentException("$it not supported")
+				}
+			}
+
+			filter.contentRating.oneOrThrowIfMany()?.let {
+				append("&adult=")
+				append(
+					when (it) {
+						ContentRating.SAFE -> "0"
+						ContentRating.ADULT -> "1"
+						else -> ""
+					},
+				)
+			}
+
+			if (filter.year != 0) {
+				append("&release=")
+				append(filter.year.toString())
+			}
+
+			append("&m_orderby=")
+			when (order) {
+				SortOrder.POPULARITY -> append("views")
+				SortOrder.UPDATED -> append("latest")
+				SortOrder.NEWEST -> append("new-manga")
+				SortOrder.ALPHABETICAL -> append("alphabet")
+				SortOrder.RATING -> append("rating")
+				SortOrder.RELEVANCE -> {}
+				else -> {}
+			}
+		}
+
+		// Use webview for Cloudflare bypass
+		val html = context.evaluateJs(url, "document.documentElement.outerHTML")
+			?: throw ParseException("Failed to load page via webview", url)
+		val doc = html.parseHtml(url)
+		return parseMangaList(doc)
+	}
+
+	override suspend fun getDetails(manga: Manga): Manga {
+		val fullUrl = manga.url.toAbsoluteUrl(domain)
+
+		// Use webview for Cloudflare bypass
+		val html = context.evaluateJs(fullUrl, "document.documentElement.outerHTML")
+			?: throw ParseException("Failed to load manga details via webview", fullUrl)
+		val doc = html.parseHtml(fullUrl)
+
+		return parseInfo(doc, manga, loadChapters(manga.url, doc))
+	}
 }

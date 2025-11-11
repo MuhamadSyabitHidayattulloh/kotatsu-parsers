d95d12901587ed2b7ee0f3dbb6ee2f5314547187
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2025-11-11 18:48:01 +0100

    Create MangaLivre.kt

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MangaLivre.kt
@@ -0,0 +1,12 @@
+package org.koitharu.kotatsu.parsers.site.madara.pt
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+
+@MangaSourceParser("MANGALIVRE", "Manga Livre", "pt")
+internal class MangaLivre(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.MANGALIVRE, "mangalivre.tv") {
+	override val datePattern = "MMMM dd, yyyy"
+}

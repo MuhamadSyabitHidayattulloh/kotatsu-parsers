8999808807479437205ed284751546448211f79f
Raphael Boullay <60440820+clarityuwu@users.noreply.github.com>
2025-11-20 08:50:49 -0200

    Update MaidScan.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MaidScan.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MaidScan.kt
@@ -3,11 +3,13 @@ package org.koitharu.kotatsu.parsers.site.madara.pt
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("MAIDSCAN", "MaidScan", "pt", ContentType.HENTAI)
 internal class MaidScan(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.MAIDSCAN, "novo.empreguetes.site", 10) {
+	MadaraParser(context, MangaParserSource.MAIDSCAN, "empreguetes.xyz", 10) {
 	override val datePattern: String = "dd/MM/yyyy"
 }

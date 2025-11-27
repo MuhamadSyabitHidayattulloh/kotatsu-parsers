1a7a26f6ccd841f14be2e497a46b87375e1f3cbe
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2025-11-27 20:02:07 +0100

    Update Mangabat.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangabox/en/Mangabat.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangabox/en/Mangabat.kt
@@ -1,5 +1,7 @@
 package org.koitharu.kotatsu.parsers.site.mangabox.en
 
+import okhttp3.Interceptor
+import okhttp3.Response
 import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
@@ -17,4 +19,20 @@ internal class Mangabat(context: MangaLoaderContext) :
     override fun getRequestHeaders() = super.getRequestHeaders().newBuilder()
         .add("referer", "https://www.mangabats.com/")
         .build()
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        // Remove encoding headers from requests to avoid gzip compression issues
+        if (originalRequest.method == "GET" || originalRequest.method == "POST") {
+            val newRequest = originalRequest.newBuilder()
+                .removeHeader("Content-Encoding")
+                .removeHeader("Accept-Encoding")
+                .build()
+            return chain.proceed(newRequest)
+        }
+
+        return chain.proceed(originalRequest)
+    }
+
 }

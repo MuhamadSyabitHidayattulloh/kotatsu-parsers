72031afe8f90d47afec716551b9244f18db4ae6d
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2025-12-02 17:19:27 +0100

    Update MiniTwoScan.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MiniTwoScan.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/MiniTwoScan.kt
@@ -8,5 +8,11 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 @MangaSourceParser("MINITWOSCAN", "MiniTwoScan", "pt")
 internal class MiniTwoScan(context: MangaLoaderContext) :
     MadaraParser(context, MangaParserSource.MINITWOSCAN, "minitwoscan.com") {
+
     override val withoutAjax = true
+
+    // Use more specific selectors to handle trailing spaces and structure
+    override val selectTestAsync = "ul.main li[class*='wp-manga-chapter']"
+    override val selectChapter = "ul.main li[class*='wp-manga-chapter']"
+
 }
